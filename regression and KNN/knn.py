34950d57302ab9b4e59ef835c5ff2eb36577867f
inspire <aakarjinwala@gmail.com>
2018-03-08 19:20:05 -0500

    regression train error and weighted knn divide by zero

--- regression and KNN/knn.py
+++ regression and KNN/knn.py
@@ -14,6 +14,12 @@ def calculate_ecludiean_distance(point1, point2):
                          + math.pow((point1[1] - point2[1]),2)) 
     return distance
 
+# Only have to consider displacement and horsepower and to predict the MPG value
+def calculate_manhattan_distance(point1, point2):
+    
+    distance = math.fabs(point1[0] - point2[0]) + math.fabs(point1[1] - point2[1]) 
+    #print(distance)
+    return distance
     
 # Function for calculating squarred error
 def calculate_squarred_error(mpg_test, mpg_predict):
@@ -27,10 +33,11 @@ def find_neighbours(train_data, test_data, k):
     distance = []
     k_near_list = []
     
-    # calculating the distance from the given point
+    # calculating the distance from the given point, forms list of tuples
     for i in range(0, len(train_data)):
         distance.append((train_data[i][0], train_data[i][1], train_data[i][2], calculate_ecludiean_distance(train_data[i],test_data)))
     
+    # tup 3 is the euclidean distance, sorting list by tuple 3
     distance.sort(key = lambda tup : tup[3]) 
 
     '''
@@ -47,34 +54,114 @@ def find_neighbours(train_data, test_data, k):
         
     mpg_predict = mpg_predict / k
     
+    '''
+    #--------- Debugging
+    
     print("\n ---- For the Test value --- %f %f %f"%(test_data[0], test_data[1], test_data[2]))
     
     for i in range(len(k_near_list)):
         print("%s : %s : %s" %(k_near_list[i][0], k_near_list[i][1],k_near_list[i][2]))
     
-    print("\n Predicted Value : %s" %(mpg_predict))
-              
+    print("\n Predicted Value : %s" %(mpg_predict))          
     print("\n")
-    
+
+    '''
     return mpg_predict    
 
 def knn_algorithm(train_data, test_data, k):
     
     test_mpg = []
     predict_mpg = []
-    
+
+    # calculating the distance from the given point, forms list of tuples    
     for i in range(0, len(test_data)):
         test_mpg.append(test_data[i][2])
         predict_mpg.append(find_neighbours(train_data, test_data[i], k))
       
+    '''
+    #--------- Debugging
     
     for i in range(len(test_mpg)):
         print(" %f : %f" %(test_mpg[i], predict_mpg[i]))
-    
+    '''
     test_error = calculate_squarred_error(test_mpg, predict_mpg)  
     
-    print("\n Test error %s" %(test_error))
+    return test_error
+
+#----------- For question 7
+
+def find_neighbours_weighted(train_data, test_data, k):
     
+    distance = []
+    k_near_list = []
+    weight_sum = float(0)
+    
+    # calculating the distance from the given point
+    for i in range(0, len(train_data)):
+        distance.append((train_data[i][0], train_data[i][1], train_data[i][2], calculate_manhattan_distance(train_data[i],test_data)))
+    
+    # tup is the manhattan distance, sorting list by tuple 3
+    distance.sort(key = lambda tup : tup[3]) 
+
+    '''
+    for i in range(len(distance)):
+        print(distance[i])    
+    '''    
+    mpg_predict = float(0)
+    
+    # for breaking ties, as of now I am just considering index value 
+    # adding the k nearest neighbour to the list
+    # taking Weighted average
+    
+    for i in range(k):
+        k_near_list.append((distance[i][0], distance[i][1], distance[i][2]))
+        
+        try:
+            inverse_distance = (1 / float(distance[i][3]))
+        except:
+            inverse_distance = 1
+            print("%s : %s : %s : %s" %(distance[i][0], distance[i][1], distance[i][2],distance[i][3]))
+            print("\n ---- For the Test value --- %f %f %f"%(test_data[0], test_data[1], test_data[2]))
+
+            print("Divide by zero error")
+
+        mpg_predict += (distance[i][2]) * inverse_distance
+        weight_sum += inverse_distance
+    
+    mpg_predict = mpg_predict / weight_sum
+    
+    '''
+    #--------- Debugging
+    print("\n ---- For the Test value --- %f %f %f"%(test_data[0], test_data[1], test_data[2]))
+    
+    for i in range(len(k_near_list)):
+        print("%s : %s : %s" %(k_near_list[i][0], k_near_list[i][1],k_near_list[i][2]))
+    
+    print("\n Predicted Value : %s" %(mpg_predict))          
+    print("\n")    
+    '''
+    return mpg_predict    
+
+def knn_algorithm_weighted(train_data, test_data, k):
+    
+    test_mpg = []
+    predict_mpg = []
+    
+    for i in range(0, len(test_data)):
+        test_mpg.append(test_data[i][2])
+        
+        # calling function for finding weighted neighbours
+        predict_mpg.append(find_neighbours_weighted(train_data, test_data[i], k))
+      
+    '''
+    #--------- Debugging
+    
+    for i in range(len(test_mpg)):
+        print(" %f : %f" %(test_mpg[i], predict_mpg[i]))    
+    '''
+    test_error = calculate_squarred_error(test_mpg, predict_mpg)  
+    
+    return test_error
     
 def main():
     
@@ -84,17 +171,27 @@ def main():
     # reading train data
     dataset_train = pd.read_csv('auto_train.csv')
 
-    knn_algorithm(dataset_train.iloc[:,0:3].values, dataset_test.iloc[:,0:3].values, 20)
-
-    knn_algorithm(dataset_train.iloc[:,0:3].values, dataset_test.iloc[:,0:3].values, 5)
 
     # K-Nearest-Neighbor
-    knn_algorithm(dataset_train.iloc[:,0:3].values, dataset_test.iloc[:,0:3].values, 1)
+    test_error1 = knn_algorithm(dataset_train.iloc[:,0:3].values, dataset_test.iloc[:,0:3].values, 1)  
+    test_error3 = knn_algorithm(dataset_train.iloc[:,0:3].values, dataset_test.iloc[:,0:3].values, 3) 
+    test_error20 = knn_algorithm(dataset_train.iloc[:,0:3].values, dataset_test.iloc[:,0:3].values, 20)
     
-    knn_algorithm(dataset_train.iloc[:,0:3].values, dataset_test.iloc[:,0:3].values, 5)
     
-    knn_algorithm(dataset_train.iloc[:,0:3].values, dataset_test.iloc[:,0:3].values, 20)
+    print("Test error for k = 1 %f" %(test_error1))
+    print("Test error for k = 3 %f" %(test_error3))
+    print("Test error for k = 20 %f" %(test_error20))
     
+    
+    # Weighted K Nearest Neighbor
+    test_error_w1 = knn_algorithm_weighted(dataset_train.iloc[:,0:3].values, dataset_test.iloc[:,0:3].values, 1) 
+    print("\n\nWeighted KNN - Test error for k = 1 %f" %(test_error_w1))
+    
+    test_error_w3 = knn_algorithm_weighted(dataset_train.iloc[:,0:3].values, dataset_test.iloc[:,0:3].values, 3)
+    print("\n\nWeighted KNN - Test error for k = 3 %f" %(test_error_w3))
+    
+    test_error_w20 = knn_algorithm_weighted(dataset_train.iloc[:,0:3].values, dataset_test.iloc[:,0:3].values, 20)
+    print("\n\nWeighted KNN - Test error for k = 20 %f" %(test_error_w20))
     
 if __name__ == "__main__":
     main()
--- regression and KNN/regression.py
+++ regression and KNN/regression.py
@@ -6,31 +6,36 @@ from sklearn.linear_model import LinearRegression
 
 from sklearn.preprocessing import PolynomialFeatures
 
-
     
 # Function for calculating squarred error
 def calculate_squarred_error(mpg_test, mpg_predict):
-    error = mpg_predict - mpg_test
-    error = np.square(error)
-    
-    sum = np.sum(error) / (error.size * 2)
-    
-    return (sum)
-
+    error_list = [math.pow(float(mpg_test[i] - mpg_predict[i]), 2) for i in range(len(mpg_predict))]
+    return float(sum(error_list)) / 2
+        
 
 # 1) plotting displacement versus mpg
 def plott_graph_one(displacement, mpg):
-    
     plt.plot(displacement, mpg, 'ro')
     plt.xlabel("displacement")
     plt.ylabel("mpg")
     plt.show()
-    
+
+# Graph plot function used for 2, 3 questions   
 def plott_graph(test, mpg_test, mpg_predict):
+    
+    # creating a list of tuple [(x,y), (x,y)]
+    mpg_tuple = zip(test, mpg_predict)
+    
+    #sorting the list of tuples based on displacement
+    mpg_tuple = sorted(mpg_tuple, key=lambda x: x[0])
+    
+    plt.xlabel("displacement")
+    plt.ylabel("mpg")
     
     # Plotting on the test data
     plt.plot(test, mpg_test,'ro')
-    plt.plot(test, mpg_predict, color = "blue")
+    
+    plt.plot(*zip(*mpg_tuple), color = "blue")
     plt.show()
     
 # 2) Simple linear regression, train a linear function to predict mpg based on displacement only       
@@ -42,20 +47,17 @@ def linear_regression(displacement_train, mpg_train, displacement_test, mpg_test
     linear_regression = LinearRegression()
     linear_regression.fit(displacement_train.reshape((291,1)),mpg_train.reshape((291,1)))
 
-    # Testing the train data
-    mpg_predict = linear_regression.predict(displacement_test)
+    # Testing  on the test data
+    mpg_predict_test = linear_regression.predict(displacement_test)
     
+    # Testing on the train data
+    mpg_predict_train = linear_regression.predict(displacement_train.reshape((291,1)))
     
-    # Plotting on the test data
-    plt.scatter(displacement_test, mpg_test, color = "red")
-    plt.plot(displacement_test, mpg_predict, color = "blue")
-    plt.show()
+    plott_graph(displacement_test, mpg_test, mpg_predict_test)
     
-    #print(mpg_predict)
-    #print(mpg_test)
-    
-    print(calculate_squarred_error(mpg_test, mpg_predict))
-    
+
+    return (calculate_squarred_error(mpg_test, mpg_predict_test), \
+             calculate_squarred_error(mpg_train, mpg_predict_train))
 
 # 3) Polynomial Regression, train 
 def poly_regression(p_train,mpg_train, p_test, mpg_test, degree):
@@ -72,19 +74,22 @@ def poly_regression(p_train,mpg_train, p_test, mpg_test, degree):
     linear_regression = LinearRegression()
     linear_regression.fit(p_train_matrix,  mpg_train.reshape(291,1))
 
-    # Testing on training data
+    # creating model using training data
     p_test_matrix = poly_regression.fit_transform(p_test.reshape(101,1))
-    mpg_predict = linear_regression.predict(p_test_matrix)
+    
+    # Getting code of mpg value and and converting array to list using ravel 
+    mpg_predict_test = linear_regression.predict(p_test_matrix).ravel()
+    
+    # Testing on the train data
+    mpg_predict_train = linear_regression.predict(p_train_matrix).ravel()
     
     #Plotting the graph
-    plott_graph(p_test, mpg_test, mpg_predict)
+    plott_graph(p_test, mpg_test, mpg_predict_test)
     
     # caclulating the squarred error
-    #print(mpg_predict)
-    print(calculate_squarred_error(mpg_test, mpg_predict))
-    
-    return 
-    
+    # print(mpg_predict)
+    return (calculate_squarred_error(mpg_test, mpg_predict_test), \
+             calculate_squarred_error(mpg_train, mpg_predict_train))
     
 # 4) multiple_linear_regression(data, train, test, )
 def multiple_linear_regression(mlr_train, mpg_train, mlr_test, mpg_test):
@@ -93,18 +98,17 @@ def multiple_linear_regression(mlr_train, mpg_train, mlr_test, mpg_test):
     linear_regression.fit(mlr_train, mpg_train.reshape((291,1)))
 
     # Testing the train data
-    mpg_predict = linear_regression.predict(mlr_test)
+    mpg_predict_test = linear_regression.predict(mlr_test)
+    mpg_predict_train = linear_regression.predict(mlr_train)
     
     #print(len(mpg_predict))
     #print(len(mpg_test))
     
-    
-    print(calculate_squarred_error(mpg_test, mpg_predict))
-    return 
-    
+    return (calculate_squarred_error(mpg_test, mpg_predict_test), \
+             calculate_squarred_error(mpg_train, mpg_predict_train))
 
 def main():    
-
+    
     mydict = {}
     
     # reading train data
@@ -112,30 +116,39 @@ def main():
 
     # reading the  train data 
     displacement_train = dataset_train.iloc[:,0].values
-    horsepower_train = dataset_train.iloc[:, 1].values
     mpg_train = dataset_train.iloc[:,2].values
-    
     
     # reading test data
     dataset_test = pd.read_csv('auto_test.csv')
     
     #reading the test data
     displacement_test = dataset_test.iloc[:,0].values
-    horsepower_test = dataset_test.iloc[:,1].values
     mpg_test = dataset_test.iloc[:,2].values
     
-    plott_graph_one(displacement_test, mpg_test)
+    
+    # 1 -- Plot Graph Test data displacement versus mpg
+    plott_graph_one(displacement_train, mpg_train)
 
     # 2 -------- Linear Regression
-    
-    # calling linear regression function
-    linear_regression(displacement_train, mpg_train, displacement_test, mpg_test)
+    mydict[0] = linear_regression(displacement_train, mpg_train, displacement_test, mpg_test)
+    print("Linear Regression Testing error %f" %(mydict[0][0]))
+    print("Linear Regression Trainning error %f" %(mydict[0][1]))
     
     # 3 ------- Polynomial Regression
-    poly_regression(displacement_train, mpg_train, displacement_test, mpg_test, 2)
-    poly_regression(displacement_train, mpg_train, displacement_test, mpg_test, 4)
-    poly_regression(displacement_train, mpg_train, displacement_test, mpg_test, 6)
+    mydict[1] = poly_regression(displacement_train, mpg_train, displacement_test, mpg_test, 2)
+    print("Polynomial Regression of degree 2 Testing error %f" %(mydict[1][0]))
+    print("Polynomial Regression of degree 2 Training error %f" %(mydict[1][1]))
     
+
+    mydict[2] = poly_regression(displacement_train, mpg_train, displacement_test, mpg_test, 4)
+    print("Polynomial Regression of degree 4 Testing error %f" %(mydict[2][0]))
+    print("Polynomial Regression of degree 4 Training error %f" %(mydict[2][1]))
+ 
+    
+    mydict[3] = poly_regression(displacement_train, mpg_train, displacement_test, mpg_test, 6)
+    print("Polynomial Regression of degree 6 Testing error %f" %(mydict[3][0]))
+    print("Polynomial Regression of degree 6 Training error %f" %(mydict[3][1]))
+ 
     
     # 4 ------- Multiple Linear Regression 
     
@@ -144,12 +157,14 @@ def main():
     mlr_test = dataset_test.iloc[:,0:2].values
  
     # calling multiple linear regression function
-    multiple_linear_regression(mlr_train, mpg_train, mlr_test, mpg_test)
+    mydict[4] = multiple_linear_regression(mlr_train, mpg_train, mlr_test, mpg_test)        
+    print("Multiple Linear Regression Testing error %f" %(mydict[4][0]))
+    print("Multiple Linear Regression Training error %f" %(mydict[4][1]))
+    
+    
     
 if __name__ == "__main__":
     main()
-
-
 
 
 
