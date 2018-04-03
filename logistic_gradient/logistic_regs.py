7fd909811197fd38b3c4fb1220f61dbdb95c453d
inspire <aakarjinwala@gmail.com>
2018-04-02 23:53:22 -0400

    partial logistic regression upto sigmoid

--- logistic_gradient/logistic_regs.py
+++ logistic_gradient/logistic_regs.py
@@ -1,29 +1,47 @@
-from IPython import get_ipython
-get_ipython().magic('reset -sf')
-
-
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import math
    
-# Function for calculating squarred error
-def calculate_squarred_error(mpg_test, mpg_predict):
-    error_list = [math.pow(float(mpg_test[i] - mpg_predict[i]), 2) for i in range(len(mpg_predict))]
-    return float(sum(error_list)) / 2
+def sigmoid(x):
+  return 1 / (1 + math.exp(-x))    
+
+# method for logistc
+def logistic_regression(df, steps, eta):
+    # w0, w1, w2, w3, ..... w60 so total 61 weights 
+    weights = [float(0.5) for i in range(len(df.columns)) ]
+
+
+    print(len(weights))
+    
+    for i in range(len(df.index)):
+    
+        # adding the first weights to the list
+        fx = weights[0]
+        
+        for y in range(len(weights) - 1):
+            # w1 corresponds i+1
+            fx += (weights[y+1] * df.iloc[i,y])
+  
+        y = sigmoid(fx)
+        
+        # updating new weights
         
 
 def main():    
     
- 
-    
+    # making pandas data frame
     df = pd.read_csv('sonar.csv')
     x = {'Mine' : 1, 'Rock' : 2}
-    
     df.iloc[:,60] = df.iloc[:,60].map(x)
     
-    print(df.iloc[:,60])
-   
+    # value for eta
+    eta = [0.001,0.01,0.05,0.1,0.5,1.0,1.5]
+    
+    for i in range(len(eta)):
+        # calling method for logistic regression
+        logistic_regression(df, 50, eta[0])
+    
 if __name__ == "__main__":
     main()
 
